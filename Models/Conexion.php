cf0e0fee556df95bd5498d995c66304cb24c3bc7
JoseLuisGAparicio <1717110268@utectulancingo.edu.mx>
2021-05-14 14:48:53 -0500

    Creacion del archivo conexion y el metodo para devolver productos

---
+++ Models/Conexion.php
@@ -0,0 +1,26 @@
+<?php
+
+    class Conexion {
+
+        private $conn;
+
+        public function __construct()
+        {
+            $this-> conn = new mysqli('localhost', 'roo', '', 'productos');
+        }
+
+        public function getProductos(){
+            $query = $this-> conn->query('SELECT * FROM productos');
+
+            $retorno = [];
+            
+            $i = 0;
+            while($fila = $query->fetch_assoc()){
+                $retorno[$i] = $fila;
+                $i++;
+            }
+
+            return $retorno;
+        }
+    }
+?>
